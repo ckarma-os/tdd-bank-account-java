a4d73c5b43a0c65ddd04adb82fa0729362c36594
ckarma-os <58325684+ckarma-os@users.noreply.github.com>
2020-07-16 19:38:33 +0530

    Added Code to create a Zero balance Account.

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -1,4 +1,11 @@
 package org.xpdojo.bank;
 
+import java.util.function.IntPredicate;
+
 public class Account {
+
+	public int balance() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 }
--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -1,15 +1,14 @@
 package org.xpdojo.bank;
 
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class AccountTest {
+import org.junit.jupiter.api.Test;
 
-    @Test
-	@Disabled
-    public void depositAnAmountToIncreaseTheBalance() {
-        assertThat("your first test").isBlank();
-    }
+public class AccountTest {
+	
+	@Test
+	public void testToCreateAnAccountWithZeroBalance() {
+		Account account = new Account();
+		assertThat(account.balance()).isEqualTo(0);
+	}
 }
